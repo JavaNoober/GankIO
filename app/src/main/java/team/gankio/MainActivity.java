557c243a879170a078bab9f8c8c853a2ed0afe2e
xiaoqi <812817883@qq.com>
2016-07-25 17:28:07 +0800

    Rejected commit: Default

--- app/src/main/java/team/gankio/MainActivity.java
+++ app/src/main/java/team/gankio/MainActivity.java
@@ -1,5 +1,6 @@
 package team.gankio;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -20,6 +21,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewPager v;
     }
 
     class MenuAdapter extends BaseAdapter{
